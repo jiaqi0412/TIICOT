a41911e2988ae2a81f637ecd5dc2955ac2293781
jiaqi0412 <lijiaqi0412@gmail.com>
2020-07-17 17:12:49 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,5 @@ app.get("/api/students", (req, res) => {
   res.send(data.students)
 })
 
-app.listen(3000, () => {console.log("Sever start at:local:3000")})
+const port = process.env.PORT || 3000
+app.listen(port, () => {console.log("Sever start at:local:3000")})
